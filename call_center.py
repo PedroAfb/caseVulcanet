085bbf1b3311958afe27ce99ca400958db970fd4
Pedro Borges <p260628@dac.unicamp.br>
2025-03-13 21:53:40 -0300

    feat(call-center): basic implementation is done

--- call_center.py
+++ call_center.py
@@ -5,26 +5,81 @@ class Operator:
         self.id = id
         self.status = "available"
 
+ANSWERED = True
+NOT_ANSWERED = False
 
 class CallCenter(cmd.Cmd):
-    def __init__(self, operators=[("A", Operator("A")), ("B", Operator("B"))]):
+    def __init__(self, operators=[Operator("A"), Operator("B")]):
         super().__init__()
         self.operators = operators
         self.queue_calls = []
         self.active_calls = dict()
 
+    def get_call_id(self, operator_id):
+        for call_id, (_, operator) in self.active_calls.items():
+            if operator.id == operator_id:
+                return call_id, operator
+        return None
 
-    def do_call(self, id):
-        pass
+    def verify_operators(self):
+        if self.queue_calls:
+            for operator in self.operators:
+                if operator.status == "available":
+                    call_id = self.queue_calls.pop(0)
+                    print("Call", call_id, "ringing for operator", operator.id)
+                    operator.status = "ringing"
+                    self.active_calls[call_id] = (NOT_ANSWERED, operator)
+                    return
+    
 
-    def do_answer(self, id):
-        pass
+    def do_call(self, call_id):
+        print("Call", call_id, "received")
 
-    def do_reject(self, id):    
-        pass
+        for operator in self.operators:
+            if operator.status == "available":
+                print("Call", call_id, "ringing for operator ", operator.id)
+                operator.status = "ringing"
+                self.active_calls[call_id] = (NOT_ANSWERED, operator)
+                return
+            
+        self.queue_calls.append(call_id)
+        print("Call", call_id, "waiting in queue")
 
-    def do_hangup(self, id):
-        pass
+
+    def do_answer(self, operator_id):
+        call_id, operator = self.get_call_id(operator_id)
+        self.active_calls[call_id] = (ANSWERED, operator)
+        print("Call", call_id, "answered by operator", operator_id)
+        operator.status = "busy"
+
+
+    def do_reject(self, operator_id):
+        call_id, operator = self.get_call_id(operator_id)
+        operator.status = "available"
+        print("Call", call_id, "rejected by operator", operator.id)
+        del self.active_calls[call_id]
+        self.queue_calls.append(call_id)
+        self.verify_operators()
+
+
+    def do_hangup(self, call_id):
+        if call_id in self.active_calls and self.active_calls[call_id][0] == ANSWERED:
+            _, operator = self.active_calls[call_id]
+            del self.active_calls[call_id]
+            operator.status = "available"
+            print("Call", call_id, "finished and operator", operator.id, "available")
+            self.verify_operators()
+        else:
+            print("Call", call_id, "missed")
+            if call_id in self.queue_calls:
+                self.queue_calls.remove(call_id)
+
+            if call_id in self.active_calls:
+                _, operator = self.active_calls[call_id]
+                operator.status = "available"
+                del self.active_calls[call_id]
+                self.verify_operators()
+
 
 if __name__ == '__main__':
     CallCenter().cmdloop()
