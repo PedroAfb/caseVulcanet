4812d08b0366b8f30618aa70c359d803749da45e
Pedro Borges <p260628@dac.unicamp.br>
2025-03-17 08:34:22 -0300

    feat(call-center): Now the implementation works like a TCP server-client

--- call_center.py
+++ call_center.py
@@ -1,13 +1,46 @@
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import ServerFactory as SvFactory
+from twisted.internet.endpoints import TCP4ServerEndpoint
+
 
 class Operator:
     def __init__(self, id):
         self.id = id
         self.status = "available"
 
+
 ANSWERED = True
 NOT_ANSWERED = False
 
-class CallCenter():
+
+class ServerFactory(SvFactory):
+    def buildProtocol(self, addr):
+        return Server()
+
+
+class Server(Protocol):
+    def connectionMade(self):
+        print("Connection made, Server")
+        self.call_center = CallCenter()
+
+    def dataReceived(self, data):
+        data = data.decode()
+        data = data.split()
+        command = data[0]
+        if command == "call":
+            msg = self.call_center.call(data[1])
+        elif command == "answer":
+            msg = self.call_center.answer(data[1])
+        elif command == "reject":
+            msg = self.call_center.reject(data[1])
+        elif command == "hangup":
+            msg = self.call_center.hangup(data[1])
+
+        self.transport.write(msg.encode())
+
+
+class CallCenter:
     def __init__(self, operators=[Operator("A"), Operator("B")]):
         self.operators = operators
         self.queue_calls = []
@@ -24,51 +57,64 @@ class CallCenter():
             for operator in self.operators:
                 if operator.status == "available":
                     call_id = self.queue_calls.pop(0)
-                    print("Call", call_id, "ringing for operator", operator.id)
                     operator.status = "ringing"
                     self.active_calls[call_id] = (NOT_ANSWERED, operator)
-                    return
-    
+                    return (
+                        "Call "
+                        + call_id
+                        + " ringing for operator "
+                        + operator.id
+                        + "\n"
+                    )
+        return ""
 
     def call(self, call_id):
-        print("Call", call_id, "received")
+        msg = "Call " + call_id + " received\n"
 
         for operator in self.operators:
             if operator.status == "available":
-                print("Call", call_id, "ringing for operator ", operator.id)
+                msg += "Call " + call_id + " ringing for operator " + operator.id + "\n"
                 operator.status = "ringing"
                 self.active_calls[call_id] = (NOT_ANSWERED, operator)
-                return
-            
-        self.queue_calls.append(call_id)
-        print("Call", call_id, "waiting in queue")
+                return msg
 
+        self.queue_calls.append(call_id)
+        msg += "Call " + call_id + " waiting in queue\n"
+        return msg
 
     def answer(self, operator_id):
         call_id, operator = self.get_call_id(operator_id)
         self.active_calls[call_id] = (ANSWERED, operator)
-        print("Call", call_id, "answered by operator", operator_id)
         operator.status = "busy"
-
+        msg = "Call " + call_id + " answered by operator " + operator_id + "\n"
+        return msg
 
     def reject(self, operator_id):
         call_id, operator = self.get_call_id(operator_id)
         operator.status = "available"
-        print("Call", call_id, "rejected by operator", operator.id)
         del self.active_calls[call_id]
         self.queue_calls.append(call_id)
-        self.verify_operators()
-
+        msg = "Call " + call_id + " rejected by operator " + operator_id + "\n"
+        msg += self.verify_operators()
+        return msg
 
     def hangup(self, call_id):
+        msg = ""
         if call_id in self.active_calls and self.active_calls[call_id][0] == ANSWERED:
             _, operator = self.active_calls[call_id]
             del self.active_calls[call_id]
             operator.status = "available"
-            print("Call", call_id, "finished and operator", operator.id, "available")
-            self.verify_operators()
+            msg = (
+                "Call "
+                + call_id
+                + " finished and operator "
+                + operator.id
+                + " available\n"
+            )
+            msg += self.verify_operators()
+            return msg
         else:
-            print("Call", call_id, "missed")
+            msg = "Call " + call_id + " missed\n"
             if call_id in self.queue_calls:
                 self.queue_calls.remove(call_id)
 
@@ -76,4 +122,11 @@ class CallCenter():
                 _, operator = self.active_calls[call_id]
                 operator.status = "available"
                 del self.active_calls[call_id]
-                self.verify_operators()
+                msg += self.verify_operators()
+            return msg
+
+
+if __name__ == "__main__":
+    endpoint = TCP4ServerEndpoint(reactor, 5678)
+    endpoint.listen(ServerFactory())
+    reactor.run()
--- command_interpreter.py
+++ command_interpreter.py
@@ -1,24 +1,52 @@
 import cmd
-from call_center import CallCenter
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import ClientFactory as CLFactory
+from twisted.internet.endpoints import TCP4ClientEndpoint
+
+
+class ClientFactory(CLFactory):
+    def buildProtocol(self, addr):
+        return Client()
+
+
+class Client(Protocol):
+    def connectionMade(self):
+        print("Connection made (client saying)")
+        reactor.callInThread(CommandInterpreter(self).cmdloop)
+
+    def dataReceived(self, data):
+        data = data.decode()
+        print(data)
+
+    def send_data(self, data):
+        self.transport.write(data.encode())
 
 
 class CommandInterpreter(cmd.Cmd):
-    def __init__(self):
+    def __init__(self, client):
         super().__init__()
-        self.call_center = CallCenter()
+        self.client = client
 
     def do_call(self, call_id):
-        self.call_center.call(call_id)
+        self.client.send_data(f"call {call_id}")
 
     def do_answer(self, operator_id):
-        self.call_center.answer(operator_id)
+        self.client.send_data(f"answer {operator_id}")
 
     def do_reject(self, operator_id):
-        self.call_center.reject(operator_id)
+        self.client.send_data(f"reject {operator_id}")
 
     def do_hangup(self, call_id):
-        self.call_center.hangup(call_id)
+        self.client.send_data(f"hangup {call_id}")
+
+    def do_exit(self, _):
+        reactor.stop()
+        return True
 
 
 if __name__ == "__main__":
-    CommandInterpreter().cmdloop()
+    factory = ClientFactory()
+    endpoint = TCP4ClientEndpoint(reactor, "localhost", 5678)
+    endpoint.connect(factory)
+    reactor.run()
